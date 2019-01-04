50363b1b63789339d5e7e51169c13373d8b51746
Jordan Mor <jordanmor@gmail.com>
2019-01-04 10:13:26 -0500

    Create temp Under Construction site for interim use

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import UnderConstruction from './underConstruction';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <UnderConstruction />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/underConstruction.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UnderConstruction = () => {
+  return ( 
+    <div className="main">
+      <h1>Under Construction</h1>
+    </div>
+   );
+}
+ 
+export default UnderConstruction;
