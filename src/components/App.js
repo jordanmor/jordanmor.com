e704c629dc2d92e7fa3c7e352971730ea8de68cb
Jordan Mor <jordanmor@gmail.com>
2019-01-08 13:55:51 -0500

    Add landing page component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Background from './background';
-import Header from './Header';
+import Landing from './landing';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 library.add(fab);
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Background />
-        <Header />
-      </React.Fragment>
+      <BrowserRouter>
+        <React.Fragment>
+          <Background />
+          <Switch>
+            <Route exact path="/" component={Landing}></Route>
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = () => {
   return ( 
-    <header className="flex-center">
-      <div className="header-main">
-        <h1 className='logo'>Hello, I'm <span>Jordan Mor!</span></h1>
-        <div className="tagline">Web Developer | Life-long Learner</div>
-        <div className="social-icons">
-          <a className="social-icon" href="https://twitter.com/imjordanmor"><FontAwesomeIcon icon={['fab', 'twitter']}/></a>
-          <a className="social-icon" href="https://www.linkedin.com/in/jordanmor/"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-          <a className="social-icon" href="https://github.com/jordanmor"><FontAwesomeIcon icon={['fab', 'github']} /></a>
-        </div>
-        <div className="button-group flex-center">
-          <button><span>Portfolio</span><i className="btn1"></i></button>
-          <button><span>About Me</span><i className="btn2"></i></button>
-          
-        </div>
-      </div>
+    <header>
     </header>
    );
 }
