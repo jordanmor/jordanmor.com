50932c608db77d9752bb6442ba09d3d323677da3
Jordan Mor <jordanmor@gmail.com>
2019-01-08 11:34:53 -0500

    Create landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import Background from './background';
 import Header from './Header';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(fab);
 
 class App extends Component {
   render() {
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = () => {
   return ( 
-    <header>
+    <header className="flex-center">
       <div className="header-main">
-        <h1 className='logo'>Hello, I'm Jordan Mor!</h1>
+        <h1 className='logo'>Hello, I'm <span>Jordan Mor!</span></h1>
         <div className="tagline">Web Developer | Life-long Learner</div>
         <div className="social-icons">
-          <a className="social-icon" href="#"></a>
-          <a className="social-icon" href="#"></a>
-          <a className="social-icon" href="#"></a>
-          <a className="social-icon" href="#"></a>
+          <a className="social-icon" href="https://twitter.com/imjordanmor"><FontAwesomeIcon icon={['fab', 'twitter']}/></a>
+          <a className="social-icon" href="https://www.linkedin.com/in/jordanmor/"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+          <a className="social-icon" href="https://github.com/jordanmor"><FontAwesomeIcon icon={['fab', 'github']} /></a>
+        </div>
+        <div className="button-group flex-center">
+          <button><span>Portfolio</span><i className="btn1"></i></button>
+          <button><span>About Me</span><i className="btn2"></i></button>
+          
         </div>
       </div>
     </header>
