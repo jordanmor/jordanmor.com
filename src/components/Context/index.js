824e773ba862c7f71046967cbbc551880c8bbd13
Jordan Mor <jordanmor@gmail.com>
2019-01-08 20:21:02 -0500

    Improve header with toggle menu & nav

--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -4,13 +4,22 @@ const PortfolioContext = React.createContext();
 
 export class Provider extends Component {
   state = { 
-    projects: []
+    projects: [],
+    menuIsOpen: false
    }
+
+  toggleMenu = () => {
+    this.setState( prevState => ({menuIsOpen: !prevState.menuIsOpen }));
+  }
 
   render() { 
     return ( 
       <PortfolioContext.Provider value={{
-        projects: this.state.projects
+        projects: this.state.projects,
+        menuIsOpen: this.state.menuIsOpen,
+        actions: {
+          toggleMenu: this.toggleMenu
+        }
       }}>
         { this.props.children }
       </PortfolioContext.Provider>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { Consumer } from '../Context';
 
 const Header = () => {
-  return ( 
-    <header>
-    </header>
-   );
+  return (
+    <Consumer>
+      { ({ menuIsOpen, actions }) => {
+        return ( 
+          <header className={ menuIsOpen ? "header open" : "header" }>
+            <div className="logo">
+              <Link to="/">
+                <h1>Jordan Mor</h1>
+              </Link>
+            </div>
+            <button onClick={ actions.toggleMenu } className="nav-icon">
+              <span className="menu-lines"></span>
+            </button>
+            <nav>
+              <NavLink to="/portfolio">Portfolio</NavLink>
+              <NavLink to="/about">About Me</NavLink>
+              <NavLink to="/contact">Contact Me</NavLink>
+            </nav>
+          </header>
+        );
+      }}
+    </Consumer>
+  );
 }
  
 export default Header;
