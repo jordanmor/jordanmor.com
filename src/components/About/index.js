e534ce3e73e1c35953a100e9da19d13d769dea78
Jordan Mor <jordanmor@gmail.com>
2019-01-16 11:36:39 -0500

    Dynamically add technologies used to each project page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,9 +14,11 @@ const About = () => {
         <h1>About Me</h1>
         <div className="card">
           <img src={profileImg} alt="Jordan Mor" />
-          <h2>Jordan Mor</h2>
-          <p className="lead">Front-end developer <span>with additonal back-end skills</span></p>
-          <p className="about-text">Hi! I'm a software developer who loves building and working with RESTful APIs and web-based applications. I'm currently concentrating on using the MERN (MongoDB, Express, React, Node.js) stack, though I have experience building web apps with SQL databases as well. I'm also currently trying to incorporate CSS Grid & Flexbox into most of my projects, though I'm also comfortable using CSS frameworks such as Bootstrap. I'm a self-motivated, lifelong learner committed to constantly improving my skills and staying current on developments and trends in the industry.</p>
+          <div>
+            <h2>Jordan Mor</h2>
+            <p className="lead">Front-end developer <span>with additonal back-end skills</span></p>
+            <p className="about-text">Hi! I'm a software developer who loves building web-based applications. I'm currently concentrating on using the MERN (MongoDB, Express, React, Node.js) stack, though I have experience building web apps with SQL databases as well. I'm also currently trying to incorporate CSS Grid & Flexbox into most of my projects, though I'm also comfortable using CSS frameworks such as Bootstrap. I'm a self-motivated, lifelong learner committed to constantly improving my skills and staying current on developments and trends in the industry.</p>
+          </div>
         </div>
         <h2>Skills</h2>
         <div className="skills">
--- src/components/Portfolio/project.js
+++ src/components/Portfolio/project.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Consumer } from '../Context';
 import { Carousel } from 'react-responsive-carousel';
+import { SVGs } from '../../assets/svgs';
 
 const Project = ({ match }) => {
   return (
@@ -8,11 +9,13 @@ const Project = ({ match }) => {
       { ({ projects }) => {
         const project = projects.filter(project => project.id === match.params.id)[0];
         const images = project.image_urls.slice(1);
+        const skills = project.technologies;
+        console.log(SVGs);
         return ( 
           <main>
             <div className="project">
               <h1>{project.project_name}</h1>
-              <Carousel showStatus={false} autoPlay interval={8000} infiniteLoop={true} >
+              <Carousel className="carousel-container" showStatus={false} autoPlay interval={8000} infiniteLoop={true} >
                 {images.map((image, index) => (
                   <div key={index} >
                     <img 
@@ -21,6 +24,24 @@ const Project = ({ match }) => {
                   </div>
                 ))}
               </Carousel>
+              <div className="project-info">
+                  <p>{project.description}</p>
+              </div>
+              <div className="skills">
+                  <h2>Technologies Used</h2>
+                  <ul>
+                    {skills.map((skill, index) => (
+                      <li key={index}>
+                        <div className="skills-icon">
+                          {SVGs[skill]}
+                        </div>
+                        <div className="skills-name">
+                          <p>{skill}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+              </div>
             </div>
           </main>
         );
