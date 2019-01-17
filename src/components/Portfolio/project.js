269ef333f2160c117cae15a27aa3c8e0c670efe4
Jordan Mor <jordanmor@gmail.com>
2019-01-16 19:36:26 -0500

    Add ability to move to next/prev project from current one

--- src/components/Portfolio/project.js
+++ src/components/Portfolio/project.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Consumer } from '../Context';
 import { Carousel } from 'react-responsive-carousel';
 import { SVGs } from '../../assets/svgs';
@@ -8,14 +9,41 @@ const Project = ({ match }) => {
     <Consumer>
       { ({ projects }) => {
         const project = projects.filter(project => project.id === match.params.id)[0];
-        const images = project.image_urls.slice(1);
+        const images = project.image_urls.slice(1); // remove the first main image from image array
         const skills = project.technologies;
-        console.log(SVGs);
+
+        let prevProjectID = '', nextProjectID = '';
+        const projectIndex = projects.indexOf(project);
+        
+        if (projectIndex === 0) {
+          prevProjectID = projects[projects.length - 1].id;
+        } else {
+          prevProjectID = projects[projectIndex - 1].id;
+        }
+        if (projectIndex === projects.length - 1) {
+          nextProjectID = projects[0].id;
+        } else {
+          nextProjectID = projects[projectIndex + 1].id;
+        }
+
         return ( 
           <main>
             <div className="project">
               <h1>{project.project_name}</h1>
-              <Carousel className="carousel-container" showStatus={false} autoPlay interval={8000} infiniteLoop={true} >
+              <div className="links">
+                <Link to={`/portfolio/project/${prevProjectID}`} className="btn">⇐ Prev Project</Link>
+                <Link to={`/portfolio/project/${nextProjectID}`} className="btn">Next Project ⇒</Link>
+              </div>
+              <Carousel className="carousel-container carousel-mobile" showStatus={false} autoPlay interval={8000} infiniteLoop={true} >
+                {images.map((image, index) => (
+                  <div key={index} >
+                    <img 
+                      src={`${image.match(/\/images\/.+\/project/g)[0]}-mobile-${index + 1}.png` } 
+                      alt={project.description} />
+                  </div>
+                ))}
+              </Carousel>
+              <Carousel className="carousel-container carousel-desktop" showStatus={false} autoPlay interval={8000} infiniteLoop={true} >
                 {images.map((image, index) => (
                   <div key={index} >
                     <img 
@@ -26,6 +54,10 @@ const Project = ({ match }) => {
               </Carousel>
               <div className="project-info">
                   <p>{project.description}</p>
+              </div>
+              <div className="links external">
+                <a href={project.live_link} className="btn">Live Demo</a>
+                <a href={project.github_link} className="btn">Github Repo</a>
               </div>
               <div className="skills">
                   <h2>Technologies Used</h2>
