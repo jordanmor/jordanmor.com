0dad10f5afb74217eef9600d1e9bbc52752411a2
Jordan Mor <jordanmor@gmail.com>
2019-01-21 18:22:14 -0500

    Style landing page for small & large screens

--- src/components/landing.js
+++ src/components/landing.js
@@ -5,17 +5,15 @@ import SocialIcons from './common/socialIcons';
 const Landing = () => {
   return ( 
     <div className="page">
-      <div className="landing">
-        <div className="container">
-          <h1 className='title'>Hello, I'm <span>Jordan Mor!</span></h1>
-          <div className="tagline">Web Developer | Life-long Learner</div>
+    <div className="landing">
+        <h1 className='title'>Hello, I'm <span>Jordan Mor!</span></h1>
+        <div className="tagline">Web Developer | Life-long Learner</div>
 
-          <SocialIcons />
-          
-          <div className="btn-group flex-center">
-            <Link to="/portfolio" className="btn flex-center"><span>Portfolio</span><i className="shine"></i></Link>
-            <Link to="/about" className="btn flex-center"><span>About Me</span><i className="shine two"></i></Link>
-          </div>
+        <SocialIcons />
+        
+        <div className="btn-group">
+          <Link to="/portfolio" className="btn"><span>Portfolio</span><i className="shine"></i></Link>
+          <Link to="/about" className="btn"><span>About Me</span><i className="shine two"></i></Link>
         </div>
       </div>
     </div>
