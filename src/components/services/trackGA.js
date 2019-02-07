1b564ceec76a1c5d5058d7c0d8435d0efe8e7a34
Jordan Mor <jordanmor@gmail.com>
2019-02-07 13:56:11 -0500

    Add google site verification meta tag and html file

--- src/components/services/trackGA.js
+++ src/components/services/trackGA.js
@@ -1,6 +1,12 @@
 import { changeTitle } from './changeTitle';
 import ReactGA from 'react-ga';
 
+/*
+The history's listen method does not fire on an initial page load or a page refresh.
+Therefore, on an initial page load or page refresh, it's necessary to include a separate
+ReactGA pageview method using window.location.pathname as the argument.
+*/
+
 export function trackGA(history) {
   // Initialize GA and declare title on first load and on page refresh
   changeTitle(window.location.pathname);
